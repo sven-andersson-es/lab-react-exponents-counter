cfca6e72601d471591a3b374f66df186c7d0495b
Sven Andersson <sven@andersson.es>
2024-11-18 21:18:17 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,29 @@ import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
-
+import { useState } from "react";
 
 function App () {
+  
+  const [count, setCount] = useState(1);
+
+ 
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter count={count} setCount={setCount}/>
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo count={count}/>
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} />
       </div>
     </div>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const decrement = () => setCount((prevCount) => prevCount - 1);
-  const increment = () => setCount((prevCount) => prevCount + 1);
+const Counter = (props) => {
+  
+  const decrement = () => props.setCount((prevCount) => prevCount - 1);
+  const increment = () => props.setCount((prevCount) => prevCount + 1);
 
   return (
     <div className="counter-container">
-      <p className="counter-value">{count}</p>
+      <p className="counter-value">{props.count}</p>
       <button className="counter-button" onClick={decrement}>-</button>
       <button className="counter-button" onClick={increment}>+</button>
     </div>
